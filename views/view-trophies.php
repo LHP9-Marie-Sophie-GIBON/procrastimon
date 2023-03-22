bac6a1c269969239b7eb612e2802a1ffe12feaad
MarieSophieGB <mariesophie.gb@gmail.com>
2023-03-22 16:57:33 +0100

    front trophy en cours

--- views/view-trophies.php
+++ views/view-trophies.php
@@ -24,32 +24,34 @@
                         </li>
                     </ul>
 
-                    <!-- Liste des goals complétées -->
-                    <?php if (empty($goalsList)) { ?>
-                        <div class="alert alert-danger" role="alert">
-                            <p class="text-center">You didn't complete any goals yet !</p>
-                        </div>
-                    <?php } else { ?>
-                        <p>Goals achieved : </p>
-                        <?php foreach ($goalsList as $goal) { ?>
-                            <div class="mb-1"><?= $goal['creation'] ?> : Goal "<?= $goal['name'] ?>" completed the <?= $goal['achievement_day'] ?> </div>
-                        <?php  }
-                    }
-                    // Liste des todos complétées
-                    if (empty($todosList)) { ?>
-                        <div class="alert alert-danger" role="alert">
-                            <p class="text-center">You didn't complete any tasks yet !</p>
-                        </div>
-                    <?php
-                    } else { ?>
-                        <p>To-Dos completed : </p>
-                        <?php foreach ($todosList as $todo) { ?>
-                            <div class="mb-1 text-muted"><?= $todo['creation'] ?> : To-do "<?= $todo['name'] ?>" </div>
-                    <?php }
-                    }
+                    <!-- Liste complète -->
+                    <div class="container History rounded-5 border border-light border-5 p-3">
+                        <?php if (empty($goalsList)) { ?>
+                            <div class="alert alert-danger" role="alert">
+                                <p class="text-center">You didn't complete any goals yet !</p>
+                            </div>
+                        <?php } else { ?>
+                            <p class="h5">Goals achieved : </p>
+                            <?php foreach ($goalsList as $goal) { ?>
+                                <div class="mb-1"><?= $goal['creation'] ?> : Goal "<?= $goal['name'] ?>" completed the <?= $goal['achievement_day'] ?> </div>
+                            <?php  }
+                        }
+                        // Liste des todos complétées
+                        if (empty($todosList)) { ?>
+                            <div class="alert alert-danger" role="alert">
+                                <p class="text-center">You didn't complete any tasks yet !</p>
+                            </div>
+                        <?php
+                        } else { ?>
+                            <p class="mt-1 h5">To-Dos completed : </p>
+                            <?php foreach ($todosList as $todo) { ?>
+                                <div class="mb-1 text-muted"><?= $todo['creation'] ?> : To-do "<?= $todo['name'] ?>" </div>
+                        <?php }
+                        } ?>
 
+                    </div>
 
-                } elseif (isset($_GET['CompletedGoals'])) { ?>
+                <?php } elseif (isset($_GET['CompletedGoals'])) { ?>
                     <!-- mise en place du menu -->
                     <ul class="nav justify-content-center">
                         <li class="nav-item">
@@ -66,12 +68,15 @@
                         </li>
                     </ul>
                     <!-- affichage des goals complétés -->
-                    <div class="col-12">
-                        <h1 class="text-center">Achieved Goals</h1>
-                        <?php foreach ($goalsList as $goal) { ?>
-                            <div class="mb-1"><?= $goal['creation'] ?> : Goal "<?= $goal['name'] ?>" completed the <?= $goal['achievement_day'] ?> </div>
-                        <?php } ?>
+                    <div class="container History rounded-5 border border-light border-5 p-3">
+                        <div class="col-12">
+                            <h1 class="text-center">Achieved Goals</h1>
+                            <?php foreach ($goalsList as $goal) { ?>
+                                <div class="mb-1"><?= $goal['creation'] ?> : Goal "<?= $goal['name'] ?>" completed the <?= $goal['achievement_day'] ?> </div>
+                            <?php } ?>
+                        </div>
                     </div>
+
 
 
                 <?php } elseif (isset($_GET['CompletedTodos'])) { ?>
@@ -91,12 +96,15 @@
                         </li>
                     </ul>
                     <!-- affichage des todos complétés -->
-                    <div class="col-12">
-                        <h1 class="text-center">Achieved todos</h1>
-                        <?php foreach ($todosList as $todo) { ?>
-                            <div class="mb-1 text-muted"><?= $todo['creation'] ?> : To-do "<?= $todo['name'] ?>" </div>
-                        <?php } ?>
+                    <div class="container History rounded-5 border border-light border-5 p-3">
+                        <div class="col-12">
+                            <h1 class="text-center">Achieved todos</h1>
+                            <?php foreach ($todosList as $todo) { ?>
+                                <div class="mb-1 text-muted"><?= $todo['creation'] ?> : To-do "<?= $todo['name'] ?>" </div>
+                            <?php } ?>
+                        </div>
                     </div>
+
 
                 <?php } elseif (isset($_GET['MissedGoals'])) { ?>
                     <!-- mise en place du menu -->
@@ -115,12 +123,15 @@
                         </li>
                     </ul>
                     <!-- affichage des goals manqués -->
-                    <div class="col-12">
-                        <h1 class="text-center">Missed Goals</h1>
-                        <?php foreach ($missedGoals as $missedGoal) { ?>
-                            <div class="mb-1"><?= $missedGoal['creation'] ?> : Goal "<?= $missedGoal['name'] ?>" missed the <?= $missedGoal['deadline'] ?> </div>
-                        <?php } ?>
+                    <div class="container History rounded-5 border border-light border-5 p-3">
+                        <div class="col-12">
+                            <h1 class="text-center">Missed Goals</h1>
+                            <?php foreach ($missedGoals as $missedGoal) { ?>
+                                <div class="mb-1"><?= $missedGoal['creation'] ?> : Goal "<?= $missedGoal['name'] ?>" </div>
+                            <?php } ?>
+                        </div>
                     </div>
+
 
                 <?php } elseif (isset($_GET['MissedTodos'])) { ?>
                     <!-- mise en place du menu -->
@@ -139,12 +150,15 @@
                         </li>
                     </ul>
                     <!-- affichage des todos manqués -->
-                    <div class="col-12">
-                        <h1 class="text-center">Missed todos</h1>
-                        <?php foreach ($missedTodos as $missedTodo) { ?>
-                            <div class="mb-1 text-muted"><?= $missedTodo['creation'] ?> : To-do "<?= $missedTodo['name'] ?>" </div>
-                        <?php } ?>
+                    <div class="container History rounded-5 border border-light border-5 p-3">
+                        <div class="col-12">
+                            <h1 class="text-center">Missed todos</h1>
+                            <?php foreach ($missedTodos as $missedTodo) { ?>
+                                <div class="mb-1 text-muted"><?= $missedTodo['creation'] ?> : To-do "<?= $missedTodo['name'] ?>" </div>
+                            <?php } ?>
+                        </div>
                     </div>
+
 
                 <?php } else if ($empty) { ?>
                     <div class="alert alert-danger" role="alert">
@@ -152,14 +166,20 @@
                     </div>
 
                 <?php } else if ($trophiesList) { ?>
-                    <?php foreach ($trophiesList as $trophy) { ?>
-                        <div class="col-2 text-center">
-                            <img src="https://img.icons8.com/fluency/48/null/trophy.png" />
-                            <p><?= $trophy['title'] ?></p>
-                            <p>(<?= $trophy['creation'] ?>)</p>
+                    <div class="container Trophies rounded-5 border border-light border-5 p-3 mt-4">
+                        <div class="row">
+                            <?php foreach ($trophiesList as $trophy) { ?>
+                                <div class="col-2 text-center">
+                                    <img src="https://img.icons8.com/fluency/48/null/trophy.png" />
+                                    <p><?= $trophy['title'] ?></p>
+                                    <p>(<?= $trophy['creation'] ?>)</p>
 
+                                </div>
+                            <?php } ?>
                         </div>
-                    <?php } ?>
+
+                    </div>
+
                 <?php } ?>
             </div>
 
